a3ea44cd0adcb7ad28654779b87ed6afc715faaa
mbarn3 <m.barnett1505@gmail.com>
2025-12-09 16:36:49 +0000

    more circles

--- sketch.js
+++ sketch.js
@@ -5,11 +5,27 @@ function setup() {
 function draw() {
     background(220);
 
-    fill(0);
-    noStroke();
+    noFill();
+    //noStroke();
 
+    for(i=0; i<17; i++){
+        for(j=0; j<12; j++)
+            circle(50*i,50*j,50);
+
+
+    }
     for(i=0; i<5; i++){
         for(j=0; j<5; j++)
-            circle(50*i,50*j,50)
+            circle(50*i,50*j,60)
+    }
+    for(i=0; i<17; i++){
+        for(j=0; j<13; j++)
+            circle(50*i,50*j,80);
+    }
+    for(i=0; i<17; i++){
+        for(j=0; j<12; j++)
+
+            circle(70*i,70*j,50)
+
     }
 }

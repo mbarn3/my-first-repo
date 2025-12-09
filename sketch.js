b6ae773f9ade7c8eb7630b84d748935c11f831be
mbarn3 <m.barnett1505@gmail.com>
2025-12-09 16:07:43 +0000

    adding circles

--- sketch.js
+++ sketch.js
@@ -4,4 +4,12 @@ function setup() {
 
 function draw() {
     background(220);
+
+    fill(0);
+    noStroke();
+
+    for(i=0; i<5; i++){
+        for(j=0; j<5; j++)
+            circle(50*i,50*j,50)
+    }
 }
